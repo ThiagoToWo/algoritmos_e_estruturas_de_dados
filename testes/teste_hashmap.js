d16050a006443588955c74686bf507c3caa1903d
ThiagoToWo <towo497@gmail.com>
2021-10-21 01:41:41 -0300

    novas funções automatizando as impressões.

--- testes/teste_hashmap.js
+++ testes/teste_hashmap.js
@@ -6,13 +6,40 @@ function log(...str) {
 	console.log(...str);
 }
 
+function logHash(key) {
+	log(key, 'hash ->',h.toHashCode(key));
+}
+
+function logValue(key) {
+	log('O valor de ' + key + ' é', h.get(key));
+}
+
+function logSize(obj) {
+	log('O tamanho do HashMap é', obj.size());
+}
+
 h.put('Thiago', 24981701997);
 h.put('João', 3299825636);
 h.put('Tereza', 2467879808);
-log('Thiago:', 'hash ->',h.toHashCode('Thiago'));
-log('João:', 'hash ->',h.toHashCode('João'));
-log('Tereza:', 'hash ->',h.toHashCode('Tereza'));
-log('O valor de Thiago é', h.get('Thiago'));
-log('O valor de João é', h.get('João'));
-log('O valor de Tereza é', h.get('Tereza'));
+h.put(497, 'p');
+h.put(undefined, 34);
+h.put(true, 'verdade');
+
+logHash('Thiago');
+logHash('João');
+logHash('Tereza');
+logHash(497);
+logHash(undefined);
+
+logSize(h);
+
+logValue('Thiago');
+logValue('João');
+logValue('Tereza');
+logValue(497);
+logValue(undefined);
+logValue(true);
+
 log('Removendo Tereza', h.remove('Tereza'));
+
+logSize(h);
