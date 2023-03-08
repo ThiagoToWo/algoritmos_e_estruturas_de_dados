54aa6dfdc098e374dad6a20dcba6eab812a0039f
ThiagoToWo <towo497@gmail.com>
2023-03-07 21:02:15 -0300

    inclusão da função BFS()

--- alg/bfs/bfs.js
+++ alg/bfs/bfs.js
@@ -30,3 +30,43 @@ export function breadthFirstSearch(graph, startVertex, callback) {
         }
     }
 }
+
+export function BFS(graph, startVertex) {
+    const vertices = graph.getVertices();
+    const adjList = graph.getAdjList();
+    const color = initializeColor(vertices);
+    const queue = new Queue();
+    const distances = {};
+    const predecessors = {};
+
+    queue.enqueue(startVertex);
+
+    for (let i = 0; i < vertices.length; i++) {
+        distances[vertices[i]] = 0;
+        predecessors[vertices[i]] = null;
+    }
+
+    while (!queue.isEmpty()) {
+        const u = queue.dequeue();
+        const nb = adjList[u];
+        color[u] = Colors.GREY;
+
+        for (let i = 0; i < nb.length; i++) {
+            const w = nb[i];
+            
+            if (color[w] === Colors.WHITE) {
+                color[w] = Colors.GREY;
+                distances[w] = distances[u] + 1;
+                predecessors[w] = u;
+                queue.enqueue(w);
+            }
+        }
+
+        color[u] = Colors.BLACK;
+    }
+
+    return {
+        distances,
+        predecessors
+    }
+}
